9f23381122b044fe159e51d81e66e64d8c1a33e3
josemariapafo <parrillajosemaria@gmail.com>
2021-03-19 12:07:08 +0100

    Añadido suma

--- src/everisGit1InicialesNombre/Main.java
+++ src/everisGit1InicialesNombre/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola José María Parrilla Forte");
-
+		System.out.println("La suma de 1 mas 1 es: "+ (1+1));
 	}
 
 }
